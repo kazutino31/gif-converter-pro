9ad52a19cf104c3adc02ab74a40020556a50adb9
macha <karumaezu361@gmail.com>
2026-02-03 04:01:45 +0900

    chore: setup gh-pages deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
   base: '/gif-converter-pro/', 
-  build: {
-    outDir: 'dist',
-  }
+  plugins: [vue()],
 })
